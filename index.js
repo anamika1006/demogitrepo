8d1608cc3abe97336ff163cd20347239c62dc4b7
Anamika Singh <anamikasinghof1026@gmail.com>
2024-05-01 15:23:19 +0530

    v1basic

---
+++ index.js
@@ -0,0 +1,12 @@
+//git add . (to add all the files that are untracked for tracking)
+let name ="Anamika";
+let surname="Singh";
+let age=22;
+
+function isAbove()
+{
+    if(age>=18)
+    {
+        return true;
+    }
+}
