a67fd40e3d1a5513d1336dc758af81a430daac38
Anamika Singh <anamikasinghof1026@gmail.com>
2024-05-07 14:16:47 +0530

    change3

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 //git add . (to add all the files that are untracked for tracking)
+// git blame [filename] to find the lines of code written by whom
+// git show f81106f(hashvalue/SHA value) to see the changes done in that version/commit.
+//git log --oneline (to shorten the hash value)
+//git reset --hard 9893196 (to rollback to the previous version)
 let name ="Anamika";
 let surname="Singh";
 let age=22;
@@ -18,4 +22,8 @@ function cacl(a,b)
 function diff(a,b)
 {
     return a-b;
+}
+function pro(a,b)
+{
+    return a*b;
 }
